44cbb551418bf04743a9a3fd6508276adf6d6637
francescazfl <francesca.zf.l@berkeley.edu>
2024-09-19 19:05:28 -0700

    add triad post

--- REVIVAL/zs/triad.py
+++ REVIVAL/zs/triad.py
@@ -30,6 +30,7 @@ class TriadData(ZSData):
         seq_dir: str = "data/seq",
         zs_dir: str = "zs",
         triad_dir: str = "triad",
+        triad_mut_dir: str = "mut_file",
         chain_id: str = "A",
     ):
 
@@ -47,6 +48,7 @@ class TriadData(ZSData):
         )
 
         self._triad_dir = checkNgen_folder(os.path.join(self._zs_dir, triad_dir))
+        self._triad_mut_dir = checkNgen_folder(os.path.join(self._triad_dir, triad_mut_dir))
         self._chain_id = chain_id
 
         self._mut_list = self._generate_mut_file()
@@ -103,8 +105,7 @@ class TriadData(ZSData):
         """
         A property for the mut file path
         """
-        sub_folder = checkNgen_folder(os.path.join(self._triad_dir, "mut_file"))
-        return os.path.join(sub_folder, f"{self.lib_name}.mut")
+        return os.path.join(self._triad_mut_dir, f"{self.lib_name}.mut")
 
     @property
     def mut_list(self) -> list:
@@ -114,8 +115,141 @@ class TriadData(ZSData):
         return self._mut_list
 
 
+class TriadResults(ZSData):
+    """
+    A class for parsing the triad results
+    """
+
+    def __init__(
+        self,
+        input_csv: str,
+        scale_fit: str = "parent",
+        combo_col_name: str = "AAs",
+        var_col_name: str = "var",
+        mut_col_name: str = "mut",
+        pos_col_name: str = "pos",
+        seq_col_name: str = "seq",
+        fit_col_name: str = "fitness",
+        seq_dir: str = "data/seq",
+        zs_dir: str = "zs",
+        triad_dir: str = "triad",
+        triad_rawouput_dir: str = "raw_output",
+        triad_processed_dir: str = "processed_output",
+        chain_id: str = "A",
+    ) -> None:
+
+        """
+        Args:
+        """
+
+        super().__init__(
+                        input_csv,
+            scale_fit,
+            combo_col_name,
+            var_col_name,
+            mut_col_name,
+            pos_col_name,
+            seq_col_name,
+            fit_col_name,
+            seq_dir,
+            zs_dir
+        )
+
+        self._triad_rawouput_dir = checkNgen_folder(os.path.join(zs_dir, triad_dir, triad_rawouput_dir))
+        self._triad_processed_dir = checkNgen_folder(os.path.join(zs_dir, triad_dir, triad_processed_dir))
+
+        print(f"Parsing {self.triad_txt} and save to {self.triad_csv}...")
+
+        # extract triad score into dataframe
+        self._triad_df = self._get_triad_score()
+
+
+    def _get_triad_score(self) -> float:
+
+        """
+        A function to load the output of a triad analysis and get a score
+
+        Args:
+        - triad_output_file: str, the path to the triad output file
+        - WT_combo: str, the wildtype combo
+        - num_seqs: int, the number of sequences to load
+        """
+
+        # Load the output file
+        with open(self.triad_txt) as f:
+
+            # Set some flags for starting analysis
+            solutions_started = False
+            record_start = False
+
+            # Begin looping over the file
+            summary_lines = []
+            for line in f:
+
+                # Start looking at data once we hit "solution"
+                # if "Solution" in line:
+                if "All sequences:" in line:
+                    solutions_started = True
+
+                # Once we have "Index" we can start recording the rest
+                if solutions_started and "Index" in line:
+                    record_start = True
+
+                # Record appropriate data
+                if record_start:
+
+                    # Strip the newline and split on whitespace
+                    summary_line = line.strip().split()
+
+                    if summary_line[0] == "Average":
+                        break
+                    else:
+                        # Otherwise, append the line
+                        summary_lines.append(summary_line)
+
+        # Build the dataframe with col ['Index', 'Tags', 'Score', 'Seq', 'Muts']
+        all_results = pd.DataFrame(summary_lines[1:], columns=summary_lines[0])
+        all_results["Triad_score"] = all_results["Score"].astype(float)
+
+        wt_chars = self.parent_aa
+        reconstructed_combos = [
+            "".join(
+                [char if char != "-" else wt_chars[i] for i, char in enumerate(seq)]
+            )
+            for seq in all_results.Seq.values
+        ]
+        all_results["AAs"] = reconstructed_combos
+
+        # Get the order
+        all_results["Triad_rank"] = np.arange(1, len(all_results) + 1)
+
+        return all_results[["AAs", "Triad_score", "Triad_rank"]].to_csv(self.triad_csv, index=False)
+
+    @property
+    def triad_txt(self) -> str:
+        """
+        A property for the triad txt
+        """
+        return os.path.join(self._triad_rawouput_dir, f"{self.lib_name}.txt")
+    
+    @property
+    def triad_csv(self) -> str:
+        """
+        A property for the triad csv
+        """
+        return os.path.join(self._triad_processed_dir, f"{self.lib_name}.csv")
+
+    @property
+    def triad_df(self) -> pd.DataFrame:
+        """
+        A property for the triad dataframe
+        """
+        return self._triad_df
+
+
 def run_traid_gen_mut_file(
-    pattern: str | list = "data/meta/scale2parent/*.csv"
+    pattern: str | list = "data/meta/scale2parent/*.csv",
+    kwargs: dict = {}
     ):
     """
     Run the triad gen mut file function for all libraries
@@ -131,6 +265,24 @@ def run_traid_gen_mut_file(
 
     for lib in lib_list:
         print(f"Running triad gen mut file for {lib}...")
-        TriadData(input_csv=lib)
+        TriadData(input_csv=lib, **kwargs)
 
 
+def run_parse_triad_results(
+    pattern: str | list = "data/meta/scale2parent/*.csv",
+    kwargs: dict = {}
+    ):
+
+    """
+    Run the parse triad results function for all libraries
+    
+    Args:
+    """
+    if isinstance(pattern, str):
+        lib_list = glob(pattern)
+    else:
+        lib_list = deepcopy(pattern)
+
+    for lib in lib_list:
+        print(f"Running parse triad results for {lib}...")
+        TriadResults(input_csv=lib, **kwargs)
---
+++ tests/test_zs-posttriad.py
@@ -0,0 +1,31 @@
+"""Test the triad pre and post processing."""
+
+import sys
+import os
+
+from glob import glob
+
+from datetime import datetime
+
+from REVIVAL.zs.triad import run_parse_triad_results
+from REVIVAL.util import checkNgen_folder
+
+if __name__ == "__main__":
+
+    # log outputs
+    f = open(
+        os.path.join(
+            checkNgen_folder("logs/zs/triad/post"),
+            f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}.out",
+        ),
+        "w",
+    )
+    sys.stdout = f
+
+    run_parse_triad_results("data/meta/scale2parent/*.csv")
+
+    """run_parse_triad_results(
+    pattern: str | list = "data/meta/scale2parent/*.csv"
+    )"""
+
+    f.close()
