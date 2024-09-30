c0c74e3cef690c4769ec34feeda2e7d615f5027b
francescazfl <francesca.zf.l@berkeley.edu>
2024-09-30 08:41:28 -0700

    update ev

--- REVIVAL/zs/ev.py
+++ REVIVAL/zs/ev.py
@@ -3,22 +3,12 @@
 from __future__ import annotations
 
 import os
-from glob import glob
+from itertools import chain
 
-
-from __future__ import annotations
-
-import os
 from glob import glob
 from copy import deepcopy
 
-import numpy as np
-import pandas as pd
-
-import torch
-
 from evcouplings.couplings import CouplingsModel
-from evcouplings.align.alignment import Alignment
 
 from REVIVAL.preprocess import ZSData
 from REVIVAL.util import checkNgen_folder
@@ -32,7 +22,7 @@ class EVData(ZSData):
     def __init__(
         self,
         input_csv: str,
-        scale_fit: str,
+        scale_fit: str = "parent",
         ev_model_dir: str = "data/evmodel",
         combo_col_name: str = "AAs",
         var_col_name: str = "var",
@@ -68,19 +58,61 @@ class EVData(ZSData):
         # check if the position is in the index map
         assert self._check_idx(), "Not all positions are in the index map!!!"
 
+        # create the subfolder
+        self._ev_dir = checkNgen_folder(os.path.join(self._zs_dir, ev_dir))
+
+        print(f"EV data will be saved at {self._ev_dir}")
+
+        # get the ev score
+        self._ev_df = self._get_evscore()
+
+        # save the ev score
+        self._ev_df.to_csv(self.ev_csv, index=False)
+
 
     def _check_idx(self):
         """Check if the position is in the index map"""
-
-        all_pos = self.df[self.pos_col_name].unique()
-        return all([p in self.idx_map for p in all_pos])
+        all_pos = set(chain.from_iterable(self.df[self._pos_col_name]))
+        return all([p in self._idx_map for p in all_pos])
 
     def _get_evscore(self):
+
+        df = deepcopy(self.df)
         
         def parse(x):
             # (self.target_positions[j], self.wt_aas[j], mut_char)
             return (int(x[1:-1]), x[0], x[-1])
 
-        self.df['ev_score'] = self.df["var_col_name"].apply(lambda x: self._model.delta_hamiltonian([parse(m) for m in x.split(':')])[0])
+        df['ev_score'] = df[self._var_col_name].apply(
+            lambda x: 0 if x == "WT" else self._model.delta_hamiltonian([parse(m) for m in x.split(':')])[0]
+        )
         
-        return self.df
+        return df
+
+    @property
+    def ev_csv(self) -> str:
+        """
+        A property for the triad csv
+        """
+        return os.path.join(self._ev_dir, f"{self.lib_name}.csv")
+
+    
+def run_all_ev(
+    pattern: str | list = "data/meta/scale2parent/*.csv",
+    kwargs: dict = {}
+    ):
+
+    """
+    Run the ev mutation scores for all the libraries
+    
+    Args:
+    """
+    if isinstance(pattern, str):
+        lib_list = glob(pattern)
+    else:
+        lib_list = deepcopy(pattern)
+
+    for lib in lib_list:
+        print(f"Getting ev zs for {lib}...")
+        EVData(input_csv=lib, **kwargs)
+
---
+++ tests/test_zs-ev.py
@@ -0,0 +1,31 @@
+"""
+Test ev mutation ZS
+"""
+
+import sys
+import os
+
+from glob import glob
+
+from datetime import datetime
+
+from REVIVAL.zs.ev import run_all_ev
+from REVIVAL.util import checkNgen_folder
+
+if __name__ == "__main__":
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+    # log outputs
+    f = open(
+        os.path.join(
+            checkNgen_folder("logs/zs/ev"),
+            f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}.out",
+        ),
+        "w",
+    )
+    sys.stdout = f
+
+    run_all_ev("data/meta/scale2parent/*.csv")
+
+    f.close()
