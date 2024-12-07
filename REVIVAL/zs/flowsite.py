93642446202dc802f63f8720170c928a524bfa79
lukasra00 <lukas.radtke@outlook.de>
2024-12-06 18:06:08 -0800

    flowsite script

--- REVIVAL/zs/flowsite.py
+++ REVIVAL/zs/flowsite.py
@@ -1,1 +1,39 @@
+# File to automatically run Flowsite as a ZS
+# Author: Lukas Radtke {lradtke@caltech.edu}
+# Date: 06/12/24
+
 import numpy as np
+import pandas as pd
+import os 
+import subprocess
+import re
+import ipdb
+import glob
+from copy import deepcopy
+from tqdm import tqdm
+from scipy import stats
+
+from REVIVAL.preprocess import ZSData
+from REVIVAL.util import checkNgen_folder
+
+
+class FlowsiteData(ZSData):
+    def __init__(
+            self,
+
+    ):
+        
+        super.__init__()
+        
+
+
+def run_flowsite(pattern: str | list = None, kwargs: dict = {}):
+
+    if isinstance(pattern, str):
+        lib_list = glob(pattern)
+    else:
+        lib_list = deepcopy(pattern)
+
+    for lib in lib_list:
+        print(f'Running Flowsite for {lib}...')
+        FlowsiteData(input_csv=lib, **kwargs)
