c1568efd2fa4ef3f480f46b93435a9c593479c15
francescazfl <francesca.zf.l@berkeley.edu>
2024-12-31 17:17:36 -0800

    add freeze cofactor opt

--- REVIVAL/zs/vina.py
+++ REVIVAL/zs/vina.py
@@ -43,7 +43,8 @@ def dock_lib_parallel(
     chai_dir: str,
     cofactor_type: str,
     residues: list = None,
-    substrate_chain_ids: list = "B",
+    substrate_chain_ids: Union[list, str] = "B",
+    freeze_opt: str = None,
     vina_dir: str = "vina",
     pH: float = 7.4,
     method="vina",
@@ -52,12 +53,14 @@ def dock_lib_parallel(
     size_z=10.0,
     num_modes=9,
     exhaustiveness=32,
+    regen=False,
     rerun=False,
     max_workers=24,  # Number of parallel workers
 ):
     """
     A function to dock all generated chai structures and get scores in parallel.
     """
+
     output_dir = checkNgen_folder(chai_dir.replace("chai/mut_structure", vina_dir))
 
     keys_to_remove = [
@@ -82,6 +85,7 @@ def dock_lib_parallel(
 
     print(f"Docking {chai_dir} with {cofactor_type} to {output_dir}")
     print(cofactor_list)
+    print(f"Freeze option: {freeze_opt}")
 
     var_paths = sorted(glob(os.path.join(chai_dir, "*", "*.cif")))
 
@@ -95,6 +99,7 @@ def dock_lib_parallel(
                 output_dir=output_dir,
                 residues=residues,
                 substrate_chain_ids=substrate_chain_ids,
+                freeze_opt=freeze_opt,
                 pH=pH,
                 method=method,
                 size_x=size_x,
@@ -102,10 +107,12 @@ def dock_lib_parallel(
                 size_z=size_z,
                 num_modes=num_modes,
                 exhaustiveness=exhaustiveness,
+                regen=regen,
                 rerun=rerun,
             )
             for var_path in var_paths
         ]
+
         for future in tqdm(as_completed(futures), total=len(var_paths)):
             try:
                 future.result()  # Raises an exception if the task failed
@@ -119,7 +126,8 @@ def dock_task(
     cofactor_list,
     output_dir,
     residues: list = None,
-    substrate_chain_ids: list = "B",
+    substrate_chain_ids: Union[list, str] = "B",
+    freeze_opt: str = None,
     pH=7.4,
     method="vina",
     size_x=10.0,
@@ -127,13 +135,21 @@ def dock_task(
     size_z=10.0,
     num_modes=9,
     exhaustiveness=32,
+    regen=False,
     rerun=False,
 ):
     """
     Task to dock a single .cif file.
     """
 
-    log_txt = glob(os.path.join(output_dir, get_file_name(var_path), "*_log.txt"))
+    if freeze_opt is None:
+        append_name = ""
+    else:
+        append_name = freeze_opt
+
+    log_txt = glob(
+        os.path.join(output_dir, get_file_name(var_path), f"*{append_name}_log.txt")
+    )
 
     var_dir = (
         os.path.normpath(
@@ -157,6 +173,7 @@ def dock_task(
             residues=residues,
             substrate_chain_ids=substrate_chain_ids,
             cofactors=cofactor_list,
+            freeze_opt=freeze_opt,
             protein_dir=var_dir,
             ligand_dir=var_dir,
             output_dir=var_dir,
@@ -167,6 +184,7 @@ def dock_task(
             size_z=size_z,
             num_modes=num_modes,
             exhaustiveness=exhaustiveness,
+            regen=regen,
         )
     except Exception as e:
         print(f"Error in docking {var_path}: {e}")
@@ -183,34 +201,45 @@ def dock(
     pH: float,
     method: str,
     residues: list = None,
-    substrate_chain_ids: list = "B",
+    substrate_chain_ids: Union[list, str] = "B",
+    freeze_opt: str = None,
     size_x=10.0,
     size_y=10.0,
     size_z=10.0,
     num_modes=9,
     exhaustiveness=32,
+    regen=False,
 ):
-    """Dock a substrate with multiple cofactors to a protein using vina."""
+    """
+    Dock a substrate with multiple cofactors to a protein using vina.
+    """
 
     # Step 1: Process the protein
     protein_name, protein_pdb_file, protein_pdbqt = format_pdb(
-        pdb_path, protein_dir, pH
+        file_path=pdb_path, protein_dir=protein_dir, pH=pH, regen=regen
     )
 
     # Step 2: Process the main ligand
-    ligand_pdbqt, _, ligand_sdf = format_ligand(smiles, ligand_name, ligand_dir, pH)
+    ligand_pdbqt, _, ligand_sdf = format_ligand(
+        smiles=smiles, name=ligand_name, ligand_dir=ligand_dir, pH=pH, regen=regen
+    )
 
     # Step 3: Process cofactors
     cofactor_pdbqts = []
     for cofactor_smiles, cofactor_name, cofactor_chainid in cofactors:
-        print(f"Processing {cofactor_name}: {cofactor_smiles}")
         cofactor_pdbqt, _, _ = format_ligand(
-            cofactor_smiles, cofactor_name, ligand_dir, pH, pdb_path, cofactor_chainid
+            smiles=cofactor_smiles,
+            name=cofactor_name,
+            ligand_dir=ligand_dir,
+            pH=pH,
+            full_pdb_path=pdb_path,
+            chain_id=cofactor_chainid,
+            regen=regen,
         )
         cofactor_pdbqts.append(cofactor_pdbqt)
 
     if method in ["vina", "ad4"]:
-        affinities = dock_vina(
+        dock_vina(
             input_pdb_path=pdb_path,
             ligand_pdbqt=ligand_pdbqt,
             protein_pdbqt=protein_pdbqt,
@@ -219,13 +248,14 @@ def dock(
             output_dir=output_dir,
             residues=residues,
             substrate_chain_ids=substrate_chain_ids,
+            cofactor_files=cofactor_pdbqts,
+            freeze_opt=freeze_opt,
             size_x=size_x,
             size_y=size_y,
             size_z=size_z,
             num_modes=num_modes,
             exhaustiveness=exhaustiveness,
             method=method,
-            cofactor_files=cofactor_pdbqts,
         )
 
     return None
@@ -239,7 +269,9 @@ def dock_vina(
     protein_name: str,
     output_dir: str,
     residues: list = None,
-    substrate_chain_ids: list = "B",
+    substrate_chain_ids: Union[list, str] = "B",
+    cofactor_files: list = None,
+    freeze_opt: str = None,
     size_x=10.0,
     size_y=10.0,
     size_z=10.0,
@@ -248,10 +280,17 @@ def dock_vina(
     method="vina",
     num_cpus: int = None,
     seed=42,
-    cofactor_files: list = None,
 ):
     """Perform docking with Vina."""
-    conf_path = os.path.join(output_dir, f"{protein_name}-{ligand_name}_conf.txt")
+
+    if freeze_opt is None:
+        append_name = ""
+    else:
+        append_name = f"-{freeze_opt}"
+
+    conf_path = os.path.join(
+        output_dir, f"{protein_name}-{ligand_name}{append_name}_conf.txt"
+    )
 
     # Step 1: Create config file
     make_config_for_vina(
@@ -261,17 +300,22 @@ def dock_vina(
         conf_path=conf_path,
         residues=residues,
         substrate_chain_ids=substrate_chain_ids,
+        cofactor_files=cofactor_files,
+        freeze_opt=freeze_opt,
         size_x=size_x,
         size_y=size_y,
         size_z=size_z,
         num_modes=num_modes,
         exhaustiveness=exhaustiveness,
-        cofactor_files=cofactor_files,
     )
 
     # Auxiliary files
-    vina_logfile = os.path.join(output_dir, f"{protein_name}-{ligand_name}_log.txt")
-    docked_ligand_pdb = os.path.join(output_dir, f"{protein_name}-{ligand_name}.pdb")
+    vina_logfile = os.path.join(
+        output_dir, f"{protein_name}-{ligand_name}{append_name}_log.txt"
+    )
+    docked_ligand_pdb = os.path.join(
+        output_dir, f"{protein_name}-{ligand_name}{append_name}.pdb"
+    )
 
     # Step 2: Perform docking
     cmd_list = [
@@ -283,6 +327,7 @@ def dock_vina(
         "--seed",
         str(seed),
     ]
+
     if num_cpus is not None:
         cmd_list += ["--cpu", str(num_cpus)]
 
@@ -293,9 +338,9 @@ def dock_vina(
         fout.write(cmd_return.stdout.decode("utf-8"))
 
     # Step 3: Convert to PDB
-    convert_pdbqt_to_pdb(
-        pdbqt_file=docked_ligand_pdb, pdb_file=docked_ligand_pdb, disable_bonding=True
-    )
+    # convert_pdbqt_to_pdb(
+    #     pdbqt_file=docked_ligand_pdb, pdb_file=docked_ligand_pdb, disable_bonding=True
+    # )
 
     return vina_logfile
 
@@ -306,15 +351,35 @@ def make_config_for_vina(
     ligand_pdbqt: str,
     conf_path: str,
     residues: list = None,
-    substrate_chain_ids: list = "B",
+    substrate_chain_ids: Union[list, str] = "B",
+    cofactor_files: list = None,
+    freeze_opt: str = None,
     size_x=10.0,
     size_y=10.0,
     size_z=10.0,
     num_modes=9,
     exhaustiveness=32,
-    cofactor_files: list = None,
 ):
-    """Create the config file for Vina, including cofactors if specified."""
+    """
+    Create the config file for Vina, including cofactors if specified.
+
+    Args:
+    - input_pdb_path (str): Path to the input PDB file.
+    - protein_pdbqt (str): Path to the protein PDBQT file.
+    - ligand_pdbqt (str): Path to the ligand PDBQT file.
+    - conf_path (str): Path to the output config file.
+    - residues (list): List of residue positions to dock.
+    - substrate_chain_ids (list): List of chain IDs for the substrate.
+    - cofactor_files (list): List of paths to cofactor PDBQT files.
+    - freeze_opt (str): Option for freezing cofactors,
+        ie. cofactor means combining their pdbqt files with the enzyme pebqt file
+            heme means just combining heme and Fe
+    - size_x (float): Size of the docking box in the x dimension.
+    - size_y (float): Size of the docking box in the y dimension.
+    - size_z (float): Size of the docking box in the z dimension.
+    - num_modes (int): Number of docking modes to generate.
+    - exhaustiveness (int): Exhaustiveness of the docking
+    """
 
     if residues is None:
         coords = calculate_chain_centroid(
@@ -330,9 +395,56 @@ def make_config_for_vina(
                 coords.append(coordinates)
         coords = calculate_centroid(coords)
 
+    main_dir = os.path.dirname(os.path.dirname(protein_pdbqt))
+
+    if freeze_opt is None:
+        receptor_pdbqt = protein_pdbqt
+        cofactor2dock = deepcopy(cofactor_files)
+
+    elif "cofactor" in freeze_opt.lower():
+        # zs/vina/PfTrpB-4bromo_cofactor/WT_0/WT_0/WT_0.pdbqt will need to become
+        # zs/vina/PfTrpB-4bromo_cofactor/WT_0/comb_cofactor.pdbqt
+        receptor_pdbqt = os.path.join(main_dir, "comb_cofactor.pdbqt")
+
+        # Combine the protein and cofactor files
+        merge_pdbqt(
+            input_files=cofactor_files + [protein_pdbqt],
+            output_file_path=receptor_pdbqt,
+        )
+
+        cofactor2dock = None
+
+    elif "heme" in freeze_opt.lower():
+        # just combine heme and Fe
+        receptor_pdbqt = os.path.join(main_dir, "comb_heme.pdbqt")
+
+        cofactor2freeze = []
+        cofactor2dock = []
+
+        for c in cofactor_files:
+            if "heme.pdbqt" in c.lower() or "fe.pdbqt" in c.lower():
+                cofactor2freeze.append(c)
+            else:
+                cofactor2dock.append(c)
+
+        # Combine the protein and cofactor files
+        merge_pdbqt(
+            input_files=cofactor2freeze + [protein_pdbqt],
+            output_file_path=receptor_pdbqt,
+        )
+
+    else:
+        raise ValueError("Invalid freeze option")
+
     with open(conf_path, "w") as fout:
-        fout.write(f"receptor = {protein_pdbqt}\n")
+        fout.write(f"receptor = {receptor_pdbqt}\n")
         fout.write(f"ligand = {ligand_pdbqt}\n")
+
+        # Include cofactors
+        if cofactor2dock is not None:
+            for cofactor_file in cofactor2dock:
+                fout.write(f"ligand = {cofactor_file}\n")
+
         fout.write(f"center_x = {coords[0]}\n")
         fout.write(f"center_y = {coords[1]}\n")
         fout.write(f"center_z = {coords[2]}\n")
@@ -341,11 +453,6 @@ def make_config_for_vina(
         fout.write(f"size_z = {size_z}\n")
         fout.write(f"num_modes = {num_modes}\n")
         fout.write(f"exhaustiveness = {exhaustiveness}\n")
-
-        # Include cofactors
-        if cofactor_files:
-            for cofactor_file in cofactor_files:
-                fout.write(f"ligand = {cofactor_file}\n")
 
 
 def write_vina_affinities(protein_name, ligand_name, log_file_path, output_csv_path):
@@ -649,7 +756,7 @@ def pdb_to_pdbqt_protein(input_path: str, output_path=None, pH: float = 7.4):
         fout.write("TER\n")
 
 
-def format_pdb(file_path: str, protein_dir: str, pH: float):
+def format_pdb(file_path: str, protein_dir: str, pH: float, regen=False):
 
     """
     Check if we have a structure,
@@ -666,13 +773,15 @@ def format_pdb(file_path: str, protein_dir: str, pH: float):
     protein_pdb_file = os.path.join(this_protein_dir, name + ".pdb")
     protein_pdbqt_file = os.path.join(this_protein_dir, name + ".pdbqt")
 
-    # Now run
-    clean_one_pdb(file_path, protein_pdb_file)
+    if not os.path.isfile(protein_pdbqt_file) or regen:
 
-    # Step 5: Convert to pdbqt for vina
-    pdb_to_pdbqt_protein(
-        input_path=protein_pdb_file, output_path=protein_pdbqt_file, pH=pH
-    )
+        # Now run
+        clean_one_pdb(file_path, protein_pdb_file)
+
+        # Step 5: Convert to pdbqt for vina
+        pdb_to_pdbqt_protein(
+            input_path=protein_pdb_file, output_path=protein_pdbqt_file, pH=pH
+        )
 
     return name, protein_pdb_file, protein_pdbqt_file
 
@@ -706,6 +815,7 @@ def format_ligand(
     pH: float,
     full_pdb_path=None,
     chain_id=None,
+    regen=False,
 ):
 
     """
@@ -721,8 +831,10 @@ def format_ligand(
     fe_pdbqt_file = os.path.join(this_ligand_dir, "Fe.pdbqt")
 
     # Return existing files if already prepared
-    if os.path.isfile(ligand_pdbqt_file):
+    if os.path.isfile(ligand_pdbqt_file) and not regen:
         return ligand_pdbqt_file, fe_pdbqt_file, ligand_sdf_file
+
+    print(f"Processing {name}: {smiles}")
 
     try:
         # Handle ions separately
@@ -742,13 +854,8 @@ def format_ligand(
             subprocess.run(
                 ["obabel", ion_pdb_file, "-O", ligand_pdbqt_file, "--metal"], check=True
             )
-            # with open(ligand_pdbqt_file, "w") as fout:
-            #     fout.write(f"ROOT\n")
-            #     fout.write(f"ATOM      1  {element_smiles}  UNL     1       0.000   0.000   0.000  1.00  0.00      {element_smiles}\n")
-            #     fout.write(f"ENDROOT\n")
-            #     fout.write(f"TORSDOF 0\n")
-        else:
 
+        else:
             # Standard ligand processing
             smiles = Chem.CanonSmiles(smiles, useChiral=True)
             protonated_smiles = protonate_smiles(smiles, pH=pH)
@@ -945,6 +1052,30 @@ def calculate_chain_centroid(input_file, chain_ids):
         return np.array(centroid).flatten()
     else:
         raise ValueError(f"No atoms found for the specified chain(s): {chain_ids}")
+
+
+def merge_pdbqt(input_files: list, output_file_path: str):
+
+    """
+    Extract and save only lines starting with ATOM from multiple files,
+    and combine them into one output file with a final TER line.
+
+    Args:
+        input_files (list): List of input file paths to be processed.
+        output_file_path (str): Path to save the combined output file.
+    """
+    with open(output_file_path, "w") as outfile:
+
+        for input_file_path in input_files:
+            with open(input_file_path, "r") as infile:
+                for line in infile:
+                    if line.startswith("ATOM"):
+                        outfile.write(line)
+
+        # Add the TER line to indicate termination
+        outfile.write("TER\n")
+
+    print(f"Combined ATOM lines saved to {output_file_path}")
 
 
 ###### extract docking scores ######
