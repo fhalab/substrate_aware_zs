9ef0fb7ae061fb9c8b2f7337e7f6c0f9e4ada97a
francescazfl <francesca.zf.l@berkeley.edu>
2025-01-02 00:12:58 -0800

    fix af3 var msa

--- REVIVAL/zs/af3.py
+++ REVIVAL/zs/af3.py
@@ -58,45 +58,24 @@ class AF3Prep(ZSData):
 
         self._gen_opt = gen_opt
 
-        # self._af3_json_dir = af3_json_dir
-        # self._af3_command_dir = af3_command_dir
-        # self._af3_struct_dir = af3_struct_dir
-
         self._af3_dir = checkNgen_folder(
             os.path.join(self._zs_dir, f"{af3_dir}_{self._gen_opt}")
         )
 
-        self._af3_msa_indir = checkNgen_folder(os.path.join(self._af3_dir, "msa_in"))
-        self._af3_msa_inpath = os.path.join(
-            self._af3_msa_indir, f"{self.protein_name}.json"
+        self._af3_msa_insubdir = checkNgen_folder(
+            os.path.join(self._af3_dir, "msa_in", self.protein_name)
         )
-        print(self._af3_msa_inpath, self.protein_name)
-        self._af3_msa_exe = os.path.join(self._af3_msa_indir, f"{self.protein_name}.sh")
-
-        self._af3_msa_dir = checkNgen_folder(os.path.join(self._af3_dir, "msa"))
-        # _data is added by af3
-        self._af3_msa_outpath = os.path.join(
-            self._af3_msa_dir, f"{self.protein_name}_data.json"
+        self._af3_msa_subdir = checkNgen_folder(
+            os.path.join(self._af3_dir, "msa", self.protein_name)
         )
 
         self._af3_json_subdir = checkNgen_folder(
             os.path.join(self._af3_dir, "json", self.lib_name)
         )
 
-        self._af3_command_subdir = checkNgen_folder(
-            os.path.join(self._af3_dir, "command", self.lib_name)
-        )
-
-        self._af3_command_path = os.path.join(
-            self._af3_command_subdir, f"run_af3_{self._gen_opt}.sh"
-        )
-
         self._af3_struct_subdir = checkNgen_folder(
             os.path.join(self._af3_dir, "struct", self.lib_name)
         )
-
-        self._full_abs_input_dir = os.path.abspath(self._af3_json_subdir)
-        self._full_abs_output_dir = os.path.abspath(self._af3_struct_subdir)
 
         self._ifrerun = ifrerun
 
@@ -114,213 +93,241 @@ class AF3Prep(ZSData):
         self._full_model_path = full_model_path
         self._full_db_path = full_db_path
 
-        self._gen_msa()
+        for (
+            var,
+            seq,
+        ) in tqdm(self.df[[self._var_col_name, self._seq_col_name]].values):
 
-        self._make_var_json()
-        self._write_var_exe()
+            rename_var = var.replace(":", "_")
 
-    def _gen_msa(self):
+            var_msa_outpath = self._gen_var_msa(var=rename_var, seq=seq)
+            self._run_var_inference(
+                var=rename_var, seq=seq, var_msa_outpath=var_msa_outpath
+            )
+
+    def _gen_var_msa(self, var, seq):
 
         """
-        A method to generate the MSA for each enzyme and then to be used for different variants.
+        A method to generate the MSA for each var, seq,
+        which can then be used for different variants.
         """
+
+        var_msa_inpath = os.path.join(self._af3_msa_insubdir, f"{var}.json")
+
+        # all cases will be lower and all : will be stripped
+        modified_var = var.lower().replace(":", "")
+
+        # _data is added by af3
+        var_msa_outpath = os.path.join(
+            self._af3_msa_subdir, modified_var, f"{modified_var}_data.json"
+        )
 
         # check if exist and ifrerun is False
-        if os.path.exists(self._af3_msa_outpath) and not self._ifrerun:
-            return
+        if os.path.exists(var_msa_outpath) and not self._ifrerun:
+            print(f"MSA for {var} already exists at {var_msa_outpath}. Skipping...")
+            return var_msa_outpath
 
         json_data = {
-            "name": f"{self.protein_name}",
-            "sequences": [{"protein": {"id": "A", "sequence": f"{self.parent_seq}"}}],
+            "name": f"{var}",
+            "sequences": [{"protein": {"id": "A", "sequence": f"{seq}"}}],
             "modelSeeds": [1],
             "dialect": "alphafold3",
             "version": 1,
         }
 
         # Save the JSON to generate msa
-        with open(self._af3_msa_inpath, "w") as json_file:
+        with open(var_msa_inpath, "w") as json_file:
             json.dump(json_data, json_file, indent=4)
 
-        # use the json for the docker run
-        with open(self._af3_msa_exe, "w") as sh_file:
-            sh_file.write("#!/bin/bash\n\n")
-
-            docker_command = f"""docker run -it \\
-                --volume {os.path.abspath(self._af3_msa_indir)}:/root/af_input \\
-                --volume {os.path.abspath(self._af3_msa_dir)}:/root/af_output \\
-                --volume {self._full_model_path}:/root/models \\
-                --volume {self._full_db_path}:/root/public_databases \\
-                --gpus all \\
-                alphafold3 \\
-                python run_alphafold.py \\
-                --json_path=/root/af_input/{os.path.basename(self._af3_msa_inpath)} \\
-                --jackhmmer_n_cpu=32 \\
-                --nhmmer_n_cpu=32 \\
-                --model_dir=/root/models \\
-                --output_dir=/root/af_output \\
-                --run_data_pipeline=True \\
-                --run_inference=False\n\n"""
-            sh_file.write(docker_command)
-
-        # Make the shell script executable
-        os.chmod(self._af3_msa_exe, 0o755)
+        # run the docker command to generate the msa
+        docker_command = [
+            "sudo",
+            "docker",
+            "run",
+            "-it",
+            # "--env", f"CUDA_VISIBLE_DEVICES={gpu_id}",
+            "--volume",
+            f"{os.path.abspath(os.path.dirname(var_msa_inpath))}:/root/af_input",
+            "--volume",
+            f"{os.path.abspath(os.path.dirname(os.path.dirname(var_msa_outpath)))}:/root/af_output",
+            "--volume",
+            f"{self._full_model_path}:/root/models",
+            "--volume",
+            f"{self._full_db_path}:/root/public_databases",
+            "--gpus",
+            "all",
+            "alphafold3",
+            "python",
+            "run_alphafold.py",
+            f"--json_path=/root/af_input/{os.path.basename(var_msa_inpath)}",
+            "--jackhmmer_n_cpu=32",
+            "--nhmmer_n_cpu=32",
+            "--model_dir=/root/models",
+            "--output_dir=/root/af_output",
+            "--run_data_pipeline=True",
+            "--run_inference=False",
+        ]
 
         # Log file for the output
-        log_file = os.path.splitext(self._af3_msa_exe)[0] + ".log"
+        log_file_path = var_msa_inpath.replace(".json", ".log")
 
-        # Execute the generated shell script
         try:
-            with open(log_file, "w") as log:
-                subprocess.run(
-                    ["sudo", self._af3_msa_exe],
-                    stdout=log,
-                    stderr=subprocess.STDOUT,
+            # Open the log file
+            with open(log_file_path, "w") as log_file:
+                # Run the Docker command using subprocess
+                result = subprocess.run(
+                    docker_command,
                     check=True,
+                    text=True,
+                    stdout=log_file,  # Redirect stdout to log file
+                    stderr=log_file,  # Redirect stderr to log file
                 )
-            print(f"MSA generation completed successfully. Logs saved to {log_file}")
+            print(
+                f"Docker command executed successfully. Logs saved to {log_file_path}"
+            )
         except subprocess.CalledProcessError as e:
-            print(f"Error during MSA generation: {e}")
-            with open(log_file, "a") as log:
-                log.write("\nDocker execution failed.\n")
+            print(
+                f"Error occurred while executing Docker command. See logs in {log_file_path}"
+            )
+            print(f"Error: {e}")
 
-    def _make_var_json(self):
+        return var_msa_outpath
+
+    def _run_var_inference(self, var_msa_outpath, var, seq):
         """
         A method to generate the AlphaFold3 JSON files for each variant.
         """
 
-        msa_json = load_json(self._af3_msa_outpath)
+        msa_json = load_json(var_msa_outpath).get("sequences", [])[0].get("protein", {})
 
-        for (
-            var,
-            seq,
-        ) in tqdm(self.df[[self._var_col_name, self._seq_col_name]].values):
+        # Create the JSON structure
+        json_data = {
+            "name": f"{var}",
+            "sequences": [
+                {
+                    "protein": {
+                        "id": "A",
+                        "sequence": f"{seq}",
+                        "modifications": msa_json.get("modifications", None),
+                        "unpairedMsa": msa_json.get("unpairedMsa", None),
+                        "pairedMsa": msa_json.get("pairedMsa", None),
+                        "templates": msa_json.get("templates", None),
+                    }
+                }
+            ],
+            "modelSeeds": [1],
+            "dialect": "alphafold3",
+            "version": 2,
+        }
 
-            # Create the JSON structure
-            json_data = {
-                "name": f"{var}",
-                "sequences": [
+        if self._gen_opt == "substrate-no-cofactor":
+
+            # add substrate
+            json_data["sequences"].append(
+                {"ligand": {"id": "B", "smiles": f"{self._sub_smiles}"}}
+            )
+
+        elif self._gen_opt == "joint-cofactor-no-substrate":
+
+            json_data["sequences"].append(
+                {"ligand": {"id": "C", "smiles": f"{self._cofactor_smiles}"}}
+            )
+
+        elif self._gen_opt == "joint-cofactor-seperate-substrate":
+
+            # add substrate
+            json_data["sequences"].append(
+                {"ligand": {"id": "B", "smiles": f"{self._sub_smiles}"}}
+            )
+
+            # add cofactor
+            json_data["sequences"].append(
+                {"ligand": {"id": "C", "smiles": f"{self._cofactor_smiles}"}}
+            )
+
+        elif self._gen_opt == "seperate":
+
+            # add substrate
+            json_data["sequences"].append(
+                {"ligand": {"id": "B", "smiles": f"{self._sub_smiles}"}}
+            )
+
+            for j, cofactor_dets, cofactor_smiles in enumerate(
+                zip(self.lib_info["cofactor"], self.lib_info["cofactor-smiles"])
+            ):
+                cofactor_smiles = canonicalize_smiles(cofactor_smiles)
+                json_data["sequences"].append(
                     {
-                        "protein": {
-                            "id": "A",
-                            "sequence": f"{seq}",
-                            "modifications": msa_json.get(
-                                "modifications", None
-                            ),
-                            "unpairedMsa": msa_json.get(
-                                "unpairedMsa", None
-                            ),
-                            "pairedMsa": msa_json.get("pairedMsa", None),
-                            "templates": msa_json.get("templates", None),
+                        "ligand": {
+                            "id": chr(
+                                67 + j
+                            ),  # 'C', 'D', etc. for additional cofactors
+                            "smiles": cofactor_smiles,
                         }
                     }
-                ],
-                "modelSeeds": [1],
-                "dialect": "alphafold3",
-                "version": 2,
-            }
-
-            if self._gen_opt == "substrate-no-cofactor":
-
-                # add substrate
-                json_data["sequences"].append(
-                    {"ligand": {"id": "B", "smiles": f"{self._sub_smiles}"}}
                 )
 
-            elif self._gen_opt == "joint-cofactor-no-substrate":
+        else:
 
-                json_data["sequences"].append(
-                    {"ligand": {"id": "C", "smiles": f"{self._cofactor_smiles}"}}
+            json_data["sequences"].append(
+                {"ligand": {"id": "B", "smiles": f"{self._joint_smiles}"}}
+            )
+
+        # Save each JSON to a separate file
+        json_file_path = os.path.join(self._af3_json_subdir, f"{var}.json")
+        with open(json_file_path, "w") as json_file:
+            json.dump(json_data, json_file, indent=4)
+            print(f"JSON file saved to {json_file_path}")
+
+        # run the docker command
+        docker_command = [
+            "sudo",
+            "docker",
+            "run",
+            "-it",
+            # "--env", f"CUDA_VISIBLE_DEVICES={gpu_id}",
+            "--volume",
+            f"{os.path.abspath(os.path.dirname(json_file_path))}:/root/af_input",
+            "--volume",
+            f"{os.path.abspath(self._af3_struct_subdir)}:/root/af_output",
+            "--volume",
+            f"{self._full_model_path}:/root/models",
+            "--volume",
+            f"{self._full_db_path}:/root/public_databases",
+            "--gpus",
+            "all",
+            "alphafold3",
+            "python",
+            "run_alphafold.py",
+            f"--json_path=/root/af_input/{os.path.basename(json_file_path)}",
+            "--model_dir=/root/models",
+            "--output_dir=/root/af_output",
+            "--run_data_pipeline=False",
+            "--run_inference=True",
+        ]
+
+        # Log file for the output
+        log_file_path = json_file_path.replace(".json", ".log")
+
+        try:
+            # Open the log file
+            with open(log_file_path, "w") as log_file:
+                # Run the Docker command using subprocess
+                result = subprocess.run(
+                    docker_command,
+                    check=True,
+                    text=True,
+                    stdout=log_file,  # Redirect stdout to log file
+                    stderr=log_file,  # Redirect stderr to log file
                 )
-
-            elif self._gen_opt == "joint-cofactor-seperate-substrate":
-
-                # add substrate
-                json_data["sequences"].append(
-                    {"ligand": {"id": "B", "smiles": f"{self._sub_smiles}"}}
-                )
-
-                # add cofactor
-                json_data["sequences"].append(
-                    {"ligand": {"id": "C", "smiles": f"{self._cofactor_smiles}"}}
-                )
-
-            elif self._gen_opt == "seperate":
-
-                # add substrate
-                json_data["sequences"].append(
-                    {"ligand": {"id": "B", "smiles": f"{self._sub_smiles}"}}
-                )
-
-                for j, cofactor_dets, cofactor_smiles in enumerate(
-                    zip(self.lib_info["cofactor"], self.lib_info["cofactor-smiles"])
-                ):
-                    cofactor_smiles = canonicalize_smiles(cofactor_smiles)
-                    json_data["sequences"].append(
-                        {
-                            "ligand": {
-                                "id": chr(
-                                    67 + j
-                                ),  # 'C', 'D', etc. for additional cofactors
-                                "smiles": cofactor_smiles,
-                            }
-                        }
-                    )
-
-            else:
-
-                json_data["sequences"].append(
-                    {"ligand": {"id": "B", "smiles": f"{self._joint_smiles}"}}
-                )
-
-            # Save each JSON to a separate file
-            json_file_path = os.path.join(self._af3_json_subdir, f"{var}.json")
-            with open(json_file_path, "w") as json_file:
-                json.dump(json_data, json_file, indent=4)
-
-    def _write_var_exe(self):
-
-        """
-        A method to write the shell script to execute the AlphaFold3 inference.
-        """
-
-        # TOOD: check if exist and ifrerun is False
-
-        with open(self._af3_command_path, "w") as sh_file:
-            sh_file.write("#!/bin/bash\n\n")
-
-            # Iterate over all JSON files
-            for json_file in os.listdir(self._af3_json_subdir):
-
-                if json_file.endswith(".json"):
-
-                    docker_command = f"""docker run -it \\
-            --volume {self._full_abs_input_dir}:/root/af_input \\
-            --volume {self._full_abs_output_dir}:/root/af_output \\
-            --volume {self._full_model_path}:/root/models \\
-            --volume {self._full_db_path}:/root/public_databases \\
-            --gpus all \\
-            alphafold3 \\
-            python run_alphafold.py \\
-            --json_path=/root/af_input/{json_file} \\
-            --jackhmmer_n_cpu=32 \\
-            --nhmmer_n_cpu=32 \\
-            --output_dir=/root/af_output \\
-            --run_data_pipeline=False \\
-            --run_inference=True\n\n"""
-                    sh_file.write(docker_command)
-
-    @property
-    def af3_dir(self) -> str:
-        """Return the directory for the AlphaFold3 directory."""
-        return self._af3_dir
-
-    @property
-    def af3_command_path(self) -> str:
-        """
-        Return the path for the shell script
-        to execute the AlphaFold3 inference for the library.
-        """
-        return self._af3_command_path
+            print(
+                f"Docker command executed successfully. Logs saved to {log_file_path}"
+            )
+        except subprocess.CalledProcessError as e:
+            print(
+                f"Error occurred while executing Docker command. See logs in {log_file_path}"
+            )
+            print(f"Error: {e}")
 
 
 def run_af3_prep(
@@ -346,24 +353,4 @@ def run_af3_prep(
 
     for lib in lib_list:
         print(f"Running AF3 for {lib}...")
-        AF3 = AF3Prep(input_csv=lib, gen_opt=gen_opt, **kwargs)
-        all_exe_paths.append(AF3.af3_command_path)
-
-    # get where this runner script will be saved
-    runner_path = os.path.join(AF3.af3_dir, f"run_af3_{gen_opt}.sh")
-
-    # write the runner script
-    with open(runner_path, "w") as runner_file:
-        runner_file.write("#!/bin/bash\n\n")
-
-        for exe_path in all_exe_paths:
-            relative_exe_path = exe_path.replace(
-                os.path.normpath(AF3.af3_dir) + "/", ""
-            )
-            runner_file.write(f"sudo bash {relative_exe_path}\n")
-
-    # Make the runner script executable
-    os.chmod(runner_path, 0o755)
-
-    # now use subprocess to run the runner script
-    subprocess.run(["sudo", runner_path], check=True)
+        AF3Prep(input_csv=lib, gen_opt=gen_opt, **kwargs)
